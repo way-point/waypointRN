324cef857b829fe8c31478f2d7f7fdd5e243b806
Ankur Ahir <ahir.ankur0429@gmail.com>
2022-09-07 18:37:05 -0700

    added ios google sign in

--- screens/SignIn/AuthButtons/index.tsx
+++ screens/SignIn/AuthButtons/index.tsx
@@ -1,58 +1,39 @@
-// import {
-//   AppleAuthenticationButton,
-//   AppleAuthenticationButtonStyle,
-//   AppleAuthenticationButtonType,
-//   AppleAuthenticationScope,
-//   signInAsync,
-// } from 'expo-apple-authentication';
-// import {maybeCompleteAuthSession} from 'expo-web-browser';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import {useAtom} from 'jotai';
 import {Pressable, Stack, Text} from 'native-base';
 // import {useAuthRequest} from 'expo-auth-session/providers/google';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Google from '../../../assets/images/SignIn/Google';
 import {currentTheme} from '../../../constants/atoms';
 
-// maybeCompleteAuthSession();
-
-// const handlePress = async () => {
-//   try {
-//     await signInAsync({
-//       requestedScopes: [
-//         AppleAuthenticationScope.FULL_NAME,
-//         AppleAuthenticationScope.EMAIL,
-//       ],
-//     });
-//   } catch (e) {
-//     console.error(e);
-//   }
-// };
+const handlePress = async () => {
+  try {
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+    const {idToken} = await GoogleSignin.signIn();
+    console.log(idToken);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 const AuthButtons = () => {
   const [currTheme] = useAtom(currentTheme);
 
-  // const [request, response, promptAsync] = useAuthRequest({
-  //   expoClientId:
-  //     '240891037955-s90rj4hlnaha4t42tf34lgru5jj0ig10.apps.googleusercontent.com',
-  //   iosClientId:
-  //     '240891037955-95es5juil94c5l8edbi2ne4okdandmve.apps.googleusercontent.com',
-  //   androidClientId:
-  //     '240891037955-gt2uk245k5lmafpl9o68id0enmuc5sa9.apps.googleusercontent.com',
-  //   webClientId:
-  //     '240891037955-mbnatgjfdaa75pillqcjsnhiusuj2dum.apps.googleusercontent.com',
-  // });
-
-  // useEffect(() => {
-  //   if (response?.type === 'success') {
-  //     const {authentication} = response;
-  //   }
-  // }, [response]);
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '240891037955-ae2ss81rvuurg7df2238rk4lves2famf.apps.googleusercontent.com',
+      iosClientId:
+        '240891037955-d0r3mnd7u2ld2dqkdl92k5iba4ge0vg9.apps.googleusercontent.com',
+      profileImageSize: 120,
+    });
+  }, []);
 
   return (
     <Stack space={4}>
       <Pressable
         onPress={() => {
-          // promptAsync();
+          handlePress();
         }}
         bg={currTheme + '.navigation.card'}
         w={230}
